334d8c869b73482f1cdb4e2c8101fa8d6dea7ad5
Teuna Ferrand <teuna.ferrand@gmail.com>
2023-12-28 05:43:53 -1000

    Add symfony/ux-twig-component

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
